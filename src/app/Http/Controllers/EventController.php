3bf3d4504021c969557d96558b152686063c80ba
Adelano <adelano.lp@outlook.com>
2022-09-15 13:54:23 -0300

    aula 30 saindo e confimando presença em eventos

--- src/app/Http/Controllers/EventController.php
+++ src/app/Http/Controllers/EventController.php
@@ -68,9 +68,22 @@ class EventController extends Controller
 
         $event = Event::findOrFail($id);
 
+        $user = auth()->user();
+        $hasUserJoined = false;
+
+        if($user) {
+            $userEvents = $user->eventsAsParticipant->toArray();
+
+            foreach($userEvents as $userEvent) {
+                if($userEvent['id'] == $id) {
+                    $hasUserJoined = true;
+                }
+            }
+        }
+
         $eventOwer = User::where('id', $event->user_id)->first()->toArray();
 
-        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwer]);
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwer, 'hasUserJoined' => $hasUserJoined]);
     }
 
     public function dashboard() {
@@ -95,7 +108,13 @@ class EventController extends Controller
 
     public function edit($id) {
 
+        $user = auth()->user();
+
         $event = Event::findOrFail($id);
+
+        if($user->id != $event->user_id) {
+            return redirect('/dashboard');
+        }
 
         return view('events.edit', ['event' => $event]);
     }
@@ -133,5 +152,16 @@ class EventController extends Controller
         $event = Event::findOrfail($id);
 
         return redirect('/dashboard')->with('msg', 'Sua presença está confirmada no evento' . $event->title);
+    }
+
+    public function leaveEvent($id) {
+
+        $user = auth()->user();
+
+        $user->eventsAsParticipant()->detach($id);
+
+        $event = Event::findOrfail($id);
+
+        return redirect('/dashboard')->with('msg', 'Você saiu com sucesso do evento:' . $event->title);
     }
 }
--- src/resources/views/events/show.blade.php
+++ src/resources/views/events/show.blade.php
@@ -14,6 +14,7 @@
                 <p class="event-city"><ion-icon name="location-outline"></ion-icon> {{ $event->city }} </p>
                 <p class="events-participants"><ion-icon name="people-outline"></ion-icon> {{ count($event->users) }} Participantes </p>
                 <p class="event-owner"><ion-icon name="star-outline"></ion-icon> {{ $eventOwner['name']}}</p>
+                @if (!$hasUserJoined)
                 <form action="/events/join/{{ $event->id }}" method="POST">
                     @csrf
                     <a href="/events/join/{{ $event->id }}"
@@ -22,7 +23,10 @@
                      onclick="event.preventDefault();
                      this.closest('form').submit();"
                      > Confirmar Presença</a>
-                </form>
+                </form>                    
+                @else
+                    <p class="already-joined-msg">Você já esta participando deste evento!</p>                    
+                @endif
                 <h3>O evento conta com:</h3>
                 <ul id="items-list">
                 @foreach ($event->items as $item)
